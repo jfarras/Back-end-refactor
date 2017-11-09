dc635acd8e447d54ba47f2da90a6dc14533b76c8
PC\Jordi <jfarras@github.com>
2017-11-09 19:57:43 +0100

    - Use of constants - sell in comparisons reduced to only execute one operation

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -5,37 +5,42 @@ namespace Runroom\GildedRose;
 class GildedRose {
 
     private $items;
+    const QUALITY_MIN    =   0;
+    const QUALITY_MAX    =   50;
+    const SELL_IN1       =   6;
+    const SELL_IN2       =   11;
+
 
     function __construct($items) {
         $this->items = $items;
     }
-
    
     function update_quality() {
         foreach ($this->items as $item) {
 
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') $item->quality--; 
+                if ($item->quality > self::QUALITY_MIN and $item->name != 'Sulfuras, Hand of Ragnaros') $item->quality--; 
            
-            } else  if ($item->quality < 50) {
+            } else  if ($item->quality < self::QUALITY_MAX) {
                     $item->quality++;
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) $item->quality++;          
-                        if ($item->sell_in < 6)  $item->quality++;       
+                        if ($item->sell_in < self::SELL_IN2 && $item->sell_in < self::SELL_IN1) $item->quality+=2;          
+                        else if ($item->sell_in < self::SELL_IN2 && $item->sell_in >= self::SELL_IN1) $item->quality++;
+
                     }
-                }
+            }
             
             if ($item->name != 'Sulfuras, Hand of Ragnaros')  $item->sell_in--;
             
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') {
+                        if ($item->quality > self::QUALITY_MIN and $item->name != 'Sulfuras, Hand of Ragnaros') {
                                 $item->quality--;
                         }
                     } else $item->quality = 0;
 
-                } else if ($item->quality < 50) $item->quality++;            
+                } else if ($item->quality < self::QUALITY_MAX) $item->quality++;            
             }
         }
     }
