9f9b926e1ad24aa0889dfc15e8082f601694ee82
PC\Jordi <jfarras@github.com>
2017-11-09 19:13:30 +0100

    - Reduced the size of the update function - Constant 0 instead of quality - quality operation

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -13,40 +13,29 @@ class GildedRose {
    
     function update_quality() {
         foreach ($this->items as $item) {
+
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality--; 
-                }
-            } else {
-                if ($item->quality < 50) {
+                if ($item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') $item->quality--; 
+           
+            } else  if ($item->quality < 50) {
                     $item->quality++;
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            $item->quality++;          
-                        }
-                        if ($item->sell_in < 6) {
-                            $item->quality++;  
-                        }
+                        if ($item->sell_in < 11) $item->quality++;          
+                        if ($item->sell_in < 6)  $item->quality++;       
                     }
                 }
-            }
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in--;
-            }
+            
+            if ($item->name != 'Sulfuras, Hand of Ragnaros')  $item->sell_in--;
+            
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') {
                                 $item->quality--;
                         }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality++;
-                    }
-                }
+                    } else $item->quality = 0;
+
+                } else if ($item->quality < 50) $item->quality++;            
             }
         }
     }
